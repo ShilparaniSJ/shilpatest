9747b124e8b9ccaf901b3cc112bfadeac6ddc3cc
ShilparaSJ <shilpasj111@gmail.com>
2019-06-14 23:33:39 +0530

    second

---
+++ shilpa/src/main/java/com/shilpa/ShilpaApplication.java
@@ -0,0 +1,13 @@
+package com.shilpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShilpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShilpaApplication.class, args);
+	}
+
+}
